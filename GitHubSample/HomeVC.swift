1ad054c5cbad34f5f5fa54f468fb9c90e04cfb10
Olamilekan <ayanphelix@gmail.com>
2018-04-05 20:53:24 +0100

    Setup homeVC and colour

---
+++ GitHubSample/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  HomeVC.swift
+//  GitHubSample
+//
+//  Created by Ayanbola Felix on 05/04/2018.
+//  Copyright © 2018 Ayanbola Felix. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = Service.primaryColor
+    }
+}
+

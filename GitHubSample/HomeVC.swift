c9b7665361af5413ed0a9904347a481c01225278
Olamilekan <ayanphelix@gmail.com>
2018-04-05 21:56:50 +0100

    Button setup

--- GitHubSample/HomeVC.swift
+++ GitHubSample/HomeVC.swift
@@ -9,11 +9,48 @@
 import UIKit
 
 class HomeVC: UIViewController {
+    
+    lazy var signinButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("LOGIN", for: .normal)
+        button.setTitleColor(Service.buttonTitleColor, for: .normal)
+        button.backgroundColor = Service.primaryDarkColor
+        button.titleLabel?.font = Service.buttonFontSize
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = Service.buttonCornerRadius
+        button.addTarget(self, action: #selector(handleSigninButton), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = Service.primaryColor
+        setupView()
+    }
+    
+    @objc fileprivate func handleSigninButton(){
+        print("login button tapped")
+    }
+    
+    private func setupView(){
+        view.addSubview(signinButton)
+        
+        signinButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, trailing: view.safeAreaLayoutGuide.trailingAnchor, bottom: nil, padding: .init(top: 16, left: 16, bottom: 0, right: 16), size: .init(width: 0, height: 50))
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
